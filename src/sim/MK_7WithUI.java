7bace4e4e9b2b8b98d82120eb8ebf5109a2543c0
Kurtis Garbutt <kurtis.garbutt@gmail.com>
2017-09-13 12:37:13 +0100

    Updated description

--- src/sim/MK_7WithUI.java
+++ src/sim/MK_7WithUI.java
@@ -20,12 +20,18 @@ import sim.util.media.chart.TimeSeriesChartGenerator;
 import agents.MainAgent;
 
 /**
- *
- * A simple model that locates agents on Norfolk's road network and makes them
- * move from A to B, then they change direction and head back to the start.
- * The process repeats until the user quits. The number of agents, their start
- * and end points is determined by data in NorfolkITNLSOA.csv and assigned by the
- * user under 'goals' (approx. Line 80 in main file).
+ * "MK_7" is the seventh iteration of my EngD project model. It is adapted from
+ * the MASON demo, "Gridlock", made by Sarah Wise, Mark Coletti, and Andrew Crooks.
+ * 
+ * The model reads a number of GIS shapefiles and displays a road network, two
+ * Environment Agency flood maps and a bespoke Open Source Vulnerability Index (OSVI).
+ * The model reads in a .CSV and generates a predetermined number of agents with set
+ * characteristics. The agents are placed on the road network and are located at
+ * a Red Cross office. The model reads a separate .CSV and assigns goal locations to 
+ * each agent at random from a predetermined list. The agents are assigned speeds at
+ * random.
+ * Once the model is started, the agents move from A to B, then they change direction
+ * and head back to their start position. The process repeats until the user quits.
  *
  * @author KJGarbutt
  *
